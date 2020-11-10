eef0e53cdfb2096e548004e4722109e2d5ca4da5
lbugasu <lbugasu@gmail.com>
2020-11-10 17:02:32 -0500

    Adding Light and Dark mode

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,7 @@ import React from "react";
 
 // import {Link} from 'react-router-dom'
 const Header = () => (
-  <div id="about" className="about">
+  <div className="about">
     <div className="left">
       <p>
         <br />
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,23 +2,52 @@ import React from "react";
 import { HashLink } from "react-router-hash-link";
 // import {Link} from 'react-router-dom'
 import Logo from "../css/images/logo.png";
+import LogoLight from "../css/images/logo-light.png";
 const Header = () => (
-  <div className="header">
-    <div className="logo">
-      <img src={Logo} alt="logo" />
+  <div>
+    <div className="header" id="top">
+      <div className="logo">
+        <HashLink id="about" smooth to="#top">
+          <img src={Logo} alt="logo" />
+        </HashLink>
+      </div>
+      <div className="menu" id="nonScrolling">
+        <span className="lightDark">
+          <span className="lightMode">🌕</span>
+          <span className="darkMode">🌑</span>
+        </span>
+        <HashLink smooth to="#about">
+          about
+        </HashLink>{" "}
+        •{" "}
+        <HashLink smooth to="#work">
+          work
+        </HashLink>{" "}
+        •{" "}
+        <HashLink smooth to="#writing">
+          writing
+        </HashLink>
+      </div>
     </div>
-    <div className="menu">
-      <HashLink smooth to="#about">
-        about
-      </HashLink>{" "}
-      •{" "}
-      <HashLink smooth to="#work">
-        work
-      </HashLink>{" "}
-      •{" "}
-      <HashLink smooth to="#writing">
-        writing
-      </HashLink>
+    <div className="header" id="scrolling">
+      <div className="logo">
+        <HashLink smooth to="#top">
+          <img src={LogoLight} alt="logo" />
+        </HashLink>
+      </div>
+      <div className="menu">
+        <HashLink smooth to="#about">
+          about
+        </HashLink>{" "}
+        •{" "}
+        <HashLink smooth to="#work">
+          work
+        </HashLink>{" "}
+        •{" "}
+        <HashLink smooth to="#writing">
+          writing
+        </HashLink>
+      </div>
     </div>
   </div>
 );
