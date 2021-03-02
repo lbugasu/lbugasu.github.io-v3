3ae7d4fc56db444d7607e6531ab34e4c46204774
Laurence Ininda <lbugasu@gmail.com>
2021-03-02 17:48:06 -0500

    Adding versioning (#7)
    * Release v1.0.1
    
    * Adding Versioning
    
    * Release v1.0.2
    
    * Release v1.0.3
    
    * Release v1.0.4
    
    * Release v1.0.5
    
    * Adding version bumping
    
    * Release v1.0.6
    
    * Release v1.0.6
    
    * Release v1.0.7
    
    Co-authored-by: runner <runner@Mac-1614724713406.local>

---
+++ Gruntfile.js
@@ -0,0 +1,42 @@
+module.exports = function (grunt) {
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON("package.json"),
+    uglify: {
+      options: {
+        banner:
+          '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n',
+      },
+      build: {
+        src: "src/<%= pkg.name %>.js",
+        dest: "build/<%= pkg.name %>.min.js",
+      },
+    },
+    bump: {
+      options: {
+        files: ["package.json"],
+        updateConfigs: [],
+        commit: true,
+        commitMessage: "Release v%VERSION%",
+        commitFiles: ["package.json"],
+        createTag: true,
+        tagName: "v%VERSION%",
+        tagMessage: "Version %VERSION%",
+        push: true,
+        pushTo: "origin",
+        gitDescribeOptions: "--tags --always --abbrev=1 --dirty=-d",
+        globalReplace: false,
+        prereleaseName: false,
+        metadata: "",
+        regExp: false,
+      },
+    },
+  });
+
+  // Load the plugin that provides the "uglify" task.
+  grunt.loadNpmTasks("grunt-contrib-uglify");
+
+  // Default task(s).
+  grunt.registerTask("default", ["uglify"]);
+  grunt.loadNpmTasks("grunt-bump");
+};
